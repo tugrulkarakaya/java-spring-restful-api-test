550a0aa9956d679363e3c16d1e82ffbea445f4c6
tugrul <tugrul.karakaya@gmail.com>
2020-04-01 04:01:11 +0100

    existing controller test shouldReturnPersonFromService modified

--- src/test/java/uk/co/huntersix/spring/rest/controller/PersonControllerTest.java
+++ src/test/java/uk/co/huntersix/spring/rest/controller/PersonControllerTest.java
@@ -1,38 +1,51 @@
 package uk.co.huntersix.spring.rest.controller;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import uk.co.huntersix.spring.rest.model.Person;
 import uk.co.huntersix.spring.rest.referencedata.PersonDataService;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PersonController.class)
 public class PersonControllerTest {
 
+    Person validPerson;
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private PersonDataService personDataService;
 
+    @BeforeEach
+    void setup() {
+        validPerson = new Person("Mary", "Smith");
+    }
+
     @Test
+    @DisplayName("Should Return Person From Service")
     public void shouldReturnPersonFromService() throws Exception {
-        when(personDataService.findPerson(any(), any())).thenReturn(new Person("Mary", "Smith"));
-        this.mockMvc.perform(get("/person/smith/mary"))
-            .andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("id").exists())
-            .andExpect(jsonPath("firstName").value("Mary"))
-            .andExpect(jsonPath("lastName").value("Smith"));
+        given(personDataService.findPerson(any(), any())).willReturn(validPerson);
+
+        MvcResult result = this.mockMvc.perform(get("/person/smith/mary"))
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("firstName").value("Mary"))
+                .andExpect(jsonPath("lastName").value("Smith"))
+                .andReturn();
+
+        System.out.println(result);
     }
 }
