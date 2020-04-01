369e903dc9bd7ce23cd2eec5722e07fd8ab10a53
tugrul <tugrul.karakaya@gmail.com>
2020-04-01 10:33:19 +0100

    Connection timeout smoke test done

--- src/test/java/uk/co/huntersix/spring/rest/controller/SmokeTest.java
+++ src/test/java/uk/co/huntersix/spring/rest/controller/SmokeTest.java
@@ -1,13 +1,16 @@
 package uk.co.huntersix.spring.rest.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-
+import org.apache.coyote.AbstractProtocol;
+import org.apache.coyote.ProtocolHandler;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SmokeTest {
@@ -30,5 +33,15 @@ public class SmokeTest {
         assertThat(this.ctx.containsBean("personDataService")).isTrue();
         assertThat(this.ctx.containsBean("personController")).isTrue();
         assertThat(this.ctx.containsBean("person")).isTrue();
+    }
+
+    @Test
+    void testTimeout() {
+        ServletWebServerApplicationContext context = (ServletWebServerApplicationContext) this.ctx;
+        TomcatWebServer embeddedServletContainer = (TomcatWebServer) context.getWebServer();
+        ProtocolHandler protocolHandler = embeddedServletContainer.getTomcat().getConnector().getProtocolHandler();
+        int timeout = ((AbstractProtocol<?>) protocolHandler).getConnectionTimeout();
+        assertThat(timeout).isEqualTo(5000);
+        //default value is 60000 updated is as 5000 in properteis file
     }
 }
