76cbb351078860bf4fa1318be3f5688379b0329f
tugrul <tugrul.karakaya@gmail.com>
2020-04-01 10:20:31 +0100

    Context Load Smoke Test implemented

--- src/test/java/uk/co/huntersix/spring/rest/controller/SmokeTest.java
+++ src/test/java/uk/co/huntersix/spring/rest/controller/SmokeTest.java
@@ -3,18 +3,32 @@ package uk.co.huntersix.spring.rest.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SmokeTest {
 
     @Autowired
     private PersonController controller;
 
+    @Autowired
+    ApplicationContext ctx;
+
     @Test
     public void contexLoads() throws Exception {
         assertThat(controller).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Contex Loaded")
+    public void checkPersonController(){
+        assertThat(this.ctx).isNotNull();
+        assertThat(this.ctx.containsBean("personDataService")).isTrue();
+        assertThat(this.ctx.containsBean("personController")).isTrue();
+        assertThat(this.ctx.containsBean("person")).isTrue();
     }
 }
